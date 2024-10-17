b6be726cf78432e4951dd6a18fc4f47a7f1a8ffa
giftdavid101 <giftdavid101@gmail.com>
2024-10-17 18:16:16 +0100

    0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- Script creates a table users with attributes
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);

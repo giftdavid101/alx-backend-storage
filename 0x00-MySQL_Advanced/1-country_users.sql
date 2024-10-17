7f510d67be1cc08ce861e1c68998b0fb0cf48373
giftdavid101 <giftdavid101@gmail.com>
2024-10-17 18:54:01 +0100

    1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Script creates table users with the attributes below
+-- id, name, email and country
+
+CREATE TABLE IF NOT EXITS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255) NOT NULL,
+	country ENUM('US',"CO","TN",) NOT NULL
+);
